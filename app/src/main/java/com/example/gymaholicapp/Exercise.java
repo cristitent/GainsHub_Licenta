38a81d405c6b4979ef2e541ddf7fa4bcbd291562
Tent Cristi <cristitent@yahoo.com>
2023-05-30 21:57:58 +0300

    Listing Exercises correctly

---
+++ app/src/main/java/com/example/gymaholicapp/Exercise.java
@@ -0,0 +1,43 @@
+package com.example.gymaholicapp;
+
+public class Exercise {
+    private String name;
+    private String type;
+    private String muscle;
+    private String equipment;
+    private String difficulty;
+    private String instructions;
+
+    public Exercise(String name, String type, String muscle, String equipment, String difficulty, String instructions) {
+        this.name = name;
+        this.type = type;
+        this.muscle = muscle;
+        this.equipment = equipment;
+        this.difficulty = difficulty;
+        this.instructions = instructions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getMuscle() {
+        return muscle;
+    }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+}
--- app/src/main/java/com/example/gymaholicapp/ExercisesActivity.java
+++ app/src/main/java/com/example/gymaholicapp/ExercisesActivity.java
@@ -2,24 +2,30 @@ package com.example.gymaholicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExercisesActivity extends AppCompatActivity {
 
     ScrollView sv_exercises;
+    LinearLayout ll_exercises;
     EditText et_exercises;
 
     @Override
@@ -29,6 +35,7 @@ public class ExercisesActivity extends AppCompatActivity {
 
         et_exercises = findViewById(R.id.et_exercises);
         sv_exercises = findViewById(R.id.sv_exercises);
+        ll_exercises = findViewById(R.id.ll_exercises);
 
         et_exercises.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,7 +44,7 @@ public class ExercisesActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         try {
-                            URL url = new URL("https://api.api-ninjas.com/v1/exercises?muscle=biceps");
+                            URL url = new URL("https://api.api-ninjas.com/v1/exercises?muscle=" + et_exercises.getText().toString());
                             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                             connection.setRequestProperty("accept", "application/json");
                             connection.setRequestProperty("X-API-Key", "xf0aeGx7IEmtEwf4eZ0hng==tSpzadwz07ocJL3C");
@@ -48,12 +55,51 @@ public class ExercisesActivity extends AppCompatActivity {
                                 ObjectMapper mapper = new ObjectMapper();
                                 JsonNode root = mapper.readTree(responseStream);
 
-                                // Process the response here
-                                final String response = root.toString();
+                                final List<Exercise> exerciseList = new ArrayList<>();
+
+                                for (JsonNode node : root) {
+                                    String name = node.get("name").asText();
+                                    String type = node.get("type").asText();
+                                    String muscle = node.get("muscle").asText();
+                                    String equipment = node.get("equipment").asText();
+                                    String difficulty = node.get("difficulty").asText();
+                                    String instructions = node.get("instructions").asText();
+
+                                    Exercise exercise = new Exercise(name, type, muscle, equipment, difficulty, instructions);
+                                    exerciseList.add(exercise);
+                                }
+
                                 runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
-                                        Toast.makeText(ExercisesActivity.this, response, Toast.LENGTH_SHORT).show();
+                                        // Clear previous views
+                                        ll_exercises.removeAllViews();
+
+                                        // Create and add views for each exercise item
+                                        for (Exercise exercise : exerciseList) {
+                                            View itemView = getLayoutInflater().inflate(R.layout.list_exercise, ll_exercises, false);
+
+                                            TextView nameTextView = itemView.findViewById(R.id.textExerciseName);
+                                            TextView typeTextView = itemView.findViewById(R.id.textExerciseType);
+                                            TextView muscleTextView = itemView.findViewById(R.id.textExerciseMuscle);
+                                            TextView equipmentTextView = itemView.findViewById(R.id.textExerciseEquipment);
+
+                                            nameTextView.setText(exercise.getName());
+                                            typeTextView.setText(exercise.getType());
+                                            muscleTextView.setText(exercise.getMuscle());
+                                            equipmentTextView.setText(exercise.getEquipment());
+
+                                            ll_exercises.addView(itemView);
+
+                                            itemView.setOnClickListener(new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View v) {
+                                                    Intent intent = new Intent(getApplicationContext(), WorkoutsActivity.class);
+                                                    startActivity(intent);
+                                                    finish();
+                                                }
+                                            });
+                                        }
                                     }
                                 });
                             } else {
